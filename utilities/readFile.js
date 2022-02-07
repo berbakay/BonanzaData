8cdf304a27eaae1663ef70329877ae19a8b5c7d8
Rick Kay <rick.kay90@gmail.com>
2022-02-07 20:48:47 +0000

    handle dirty Leeds edge case

--- utilities/readFile.js
+++ utilities/readFile.js
@@ -9,7 +9,8 @@ const readFixture = () => {
         if (err) console.log(err);
         else {
             let splitFixtures = data.split('\n')
-    
+            
+            //this is getting rid of the blank fixtures as txt files have blank lines which are split above
             let noSpaceFilter = splitFixtures.filter(fixture => fixture != " ");
             let noBlankFixtures = noSpaceFilter.filter(fixture => fixture != "");
     
@@ -26,20 +27,28 @@ const readFixture = () => {
                 'Man United': 'Manchester United'
             }
     
+            //if fixtures contains any of the keys replace with full team name
             noBlankFixtures.forEach(fixture => {
                 Object.keys(teams).forEach(team => {
                     if (fixture.includes(team)) {
-                        const newFixture = fixture.replace(team, teams[team]);
+
+                        let newFixture = fixture.replace(team, teams[team]);
+                        
+                        //handle dirty Leeds edge case
+                        if (newFixture.includes('Leeds United United')) {
+                            newFixture = newFixture.replace('Leeds United United', 'Leeds United');
+                        };
+
                         noBlankFixtures[noBlankFixtures.indexOf(fixture)] = newFixture;
                         fixture = newFixture;
                     }
                 })
             })
             
-            
             noBlankFixtures.forEach((fixture, i) => {
                 fixturesArray[i] = fixture;
             })
+            console.log(noBlankFixtures);
             parseData(noBlankFixtures);
         }
     })
